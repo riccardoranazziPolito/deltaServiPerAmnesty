29f8ff44ddc29668e672596b59ddab1466a25601
RICCARDO RANAZZI s359331 <s359331@studenti.polito.it>
2026-08-19 19:50:09 +0200

    Ready for production deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
-const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' })
-const prisma = new PrismaClient({ adapter })
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
